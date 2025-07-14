ac1156fd4472cd121290de17d4f25880daa9af5a
Parashar Deb <parashardeb11@gmail.com>
2025-07-14 23:58:24 +0530

    singup end point

--- apps/http_server/src/user.ts
+++ apps/http_server/src/user.ts
@@ -1,18 +1,35 @@
 import express, { Router } from "express"
 import SignupSchema from "./zod";
-
+import { prismaclient } from "@repo/db/client";
 const userrouter:Router=express.Router();
-userrouter.post("/signup",async(req,res)=>{
-    const parseddata=SignupSchema.safeParse(req.body)
-    if(!parseddata){
-        res.json({
-            message:"Invalid Input"
-        })
-        return
-    }
+userrouter.post("/signup", async (req, res) => {
+  const parseddata = SignupSchema.safeParse(req.body);
 
-    res.json({
+  if (!parseddata.success) {
+    return res.status(400).json({
+      message: "Invalid Input"
+    });
+  }
 
-    })
-})
+  const { username, email, password } = parseddata.data;
+
+  try {
+    const user = await prismaclient.user.create({
+      data: {
+        username,
+        email,
+        password
+      }
+    });
+    res.json({ message: "User created", user });
+  } catch (err) {
+    res.status(411).json({
+      message: "Email already exists"
+    });
+  }
+  res.json({
+    message:"you are signed up"
+  })
+});
+
 export default userrouter
