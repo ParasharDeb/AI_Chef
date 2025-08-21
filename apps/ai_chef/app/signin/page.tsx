ec1be60b87b25fb83a5d160d9b4c62c2318d62f0
ParasharDeb <parashardeb11@gmail.com>
2025-08-21 21:53:26 +0530

    fixed signup and signin page

--- apps/ai_chef/app/signin/page.tsx
+++ apps/ai_chef/app/signin/page.tsx
@@ -1,29 +1,38 @@
 'use client'
+import Link from 'next/link';
 import Buttoncustom from "@/ui/button";
 import InputBox from "@/ui/inputbox";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios"
+
 export default function Signup(){
-    const [password,setpassword]=useState("")
-    const [email,setemail]=useState("")
-    const Router=useRouter();
+    const [password, setpassword] = useState("")
+    const [email, setemail] = useState("")
+    const Router = useRouter();
     return(
         <div className="bg-gray-200 w-screen h-screen flex items-center justify-center">
-            <div className="bg-white h-100 w-80 rounded-2xl px-10 py-10 shadow-lg">
+            <div className="bg-white h-90 w-80 rounded-2xl px-10 py-10 shadow-lg">
                 <h1 className="text-bold px-20 pb-5">Sign In</h1>
-                <InputBox value={password} inputtype="password" inputplaceholder="enter your password" onchangehandler={(e)=>{setpassword(e.target.value)}}/>
-                <InputBox value={email} inputplaceholder="enter your email" inputtype="text" onchangehandler={(e)=>{setemail(e.target.value)}}/>
+                <InputBox value={email} inputplaceholder="enter your email" inputtype="text" onchangehandler={(e) => setemail(e.target.value)} />
+                <InputBox value={password} inputtype="password" inputplaceholder="enter your password" onchangehandler={(e) => setpassword(e.target.value)} />
+
                 <Buttoncustom name="Signin" onClickhandler={
-                        async() => {
+                    async () => {
                         await axios.post("http://localhost:3001/api/v1/user/signin", {
                             email,
                             password,
                         }).then((response) => {
-                            Router.push("/")
-                            localStorage.setItem("token",response.data.token) 
-                        })
-                    }}/>
+                            Router.push("/");
+                            localStorage.setItem("token", response.data.token);
+                        });
+                    }} />
+                <h6 className="pt-2 px-2">
+                    Don&apos;t have an account?&nbsp;
+                    <Link href="/signup" className="text-blue-600 underline hover:text-blue-800">
+                        Signup
+                    </Link>
+                </h6>
             </div>
         </div>
     )
--- apps/ai_chef/app/signup/page.tsx
+++ apps/ai_chef/app/signup/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import Buttoncustom from "@/ui/button";
 import InputBox from "@/ui/inputbox";
+import Link from "next/link";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios"
@@ -11,7 +12,7 @@ export default function Signup(){
     const Router=useRouter();
     return(
         <div className="bg-gray-200 w-screen h-screen flex items-center justify-center">
-            <div className="bg-white h-100 w-80 rounded-2xl px-10 py-10 shadow-lg">
+            <div className="bg-white h-105 w-80 rounded-2xl px-10 py-10 shadow-lg">
                 <h1 className="text-bold px-20 pb-5">Sign Up</h1>
                 <InputBox value={username} inputtype="text" inputplaceholder="enter your username" onchangehandler={(e)=>{setusername(e.target.value)}}/>
                 <InputBox value={password} inputtype="password" inputplaceholder="enter your password" onchangehandler={(e)=>{setpassword(e.target.value)}}/>
@@ -26,6 +27,7 @@ export default function Signup(){
                             Router.push("/signin")
                         })
                     }}/>
+                <h6 className="py-4 px-2">Already have an accout? <Link href="/signin" className="text-blue-600 cursor-pointer hover:text-blue-800">Sign in</Link></h6>
             </div>
         </div>
     )
