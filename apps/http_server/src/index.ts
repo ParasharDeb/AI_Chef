8ecd7f4f56ac132e9a1af3d6959a2e6c58f537da
ParasharDeb <parashardeb11@gmail.com>
2025-08-21 19:47:30 +0530

    stitched backend

--- apps/http_server/src/index.ts
+++ apps/http_server/src/index.ts
@@ -2,9 +2,10 @@ import express from "express";
 import userrouter from "./user";
 import reciperouter from "./recipe";
 import airouter from "./ai";
-
+import cors from 'cors'
 const app = express();
 app.use(express.json());
+app.use(cors())
 const routes=express.Router();
 app.use("/api/v1",routes)
 routes.use("/user",userrouter),
