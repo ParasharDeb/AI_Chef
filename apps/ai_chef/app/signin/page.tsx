0a36d924406ec4341a2269eeb8721e272267dd38
ParasharDeb <parashardeb11@gmail.com>
2025-09-13 00:35:41 +0530

    stiched signin endpoint

--- apps/ai_chef/app/signin/page.tsx
+++ apps/ai_chef/app/signin/page.tsx
@@ -2,16 +2,18 @@
 import {Button} from "@repo/ui/button"
 import { Inputbox } from "@repo/ui/input"
 import axios from "axios"
-
+import { useRouter } from "next/navigation"
 import { useState } from "react"
 export default function Signup(){
     const [email,setemail]=useState("")
     const [password,setpassword]=useState("")
+    const Router = useRouter()
     function Singin(){
         axios.post("http://localhost:8080/api/v1/user/signin",{
             email,password
         }).then(
             Response=>{
+                Router.push("/")
                 console.log(Response)
             }
         ).catch(error=>{
@@ -21,10 +23,10 @@ export default function Signup(){
     return(
         <div className="bg-paper flex w-screen h-screen justify-center items-center">
             <div className="isolate aspect-video w-96 h-fit rounded-xl bg-white/20 shadow-lg ring-1 ring-black/5 px-5 py-10">
-                <div className="text-[#ff7a29] font-bold text-4xl font-mono ml-25">Signup</div>
-                <Inputbox Changehandler={(e)=>{setemail(e.target.value)}} type="text" placeholder="password" Title="Password"/>
+                <div className="text-[#ff7a29] font-bold text-4xl font-mono ml-25">Signin</div>
                 <Inputbox Changehandler={(e)=>{setpassword(e.target.value)}} type="text" placeholder="Email" Title="Email"/>
-                <Button children="Signup" clickhandler={Singin}/>
+                <Inputbox Changehandler={(e)=>{setemail(e.target.value)}} type="text" placeholder="password" Title="Password"/>
+                <Button children="Signin" clickhandler={Singin}/>
                 {/* SHould have a footer  */}
                 <div>
 
--- apps/ai_chef/app/signup/page.tsx
+++ apps/ai_chef/app/signup/page.tsx
@@ -2,11 +2,13 @@
 import {Button} from "@repo/ui/button"
 import { Inputbox } from "@repo/ui/input"
 import { useState } from "react"
+import { useRouter } from "next/navigation"
 import axios from "axios"
 export default function Signup(){
     const [username,setusername]=useState("")
     const [password,setpassword]=useState("")
     const [email,setemail]=useState("")
+    const Router=useRouter()
     return(
         <div className="bg-paper flex w-screen h-screen justify-center items-center">
             <div className="isolate aspect-video w-96 h-fit rounded-xl bg-white/20 shadow-lg ring-1 ring-black/5 px-5 py-10">
@@ -15,6 +17,7 @@ export default function Signup(){
                 <Inputbox Changehandler={(e)=>{setpassword(e.target.value)}} type="password" placeholder="password" Title="Password"/>
                 <Inputbox Changehandler={(e)=>{setemail(e.target.value)}} type="text" placeholder="Email" Title="Email"/>
                 <Button children="Signup" clickhandler={()=>{axios.post("http://localhost:8080/api/v1/user/signup",{username,password,email}).then(Response=>{
+                    Router.push("/signin")
                     console.log(Response)
                 }).catch(error=>{
                     console.log(error)
