0edb928f8bfe274425cca2ebcd4df8330ef0ee71
ParasharDeb <parashardeb11@gmail.com>
2025-09-12 23:43:06 +0530

    added signin signup page and custon intput and button component

--- apps/ai_chef/app/signup/page.tsx
+++ apps/ai_chef/app/signup/page.tsx
@@ -1,9 +1,19 @@
-import button from "@repo/ui"
+'use client'
+import {Button} from "@repo/ui/button"
+import { Inputbox } from "@repo/ui/input"
 export default function Signup(){
     return(
         <div className="bg-paper flex w-screen h-screen justify-center items-center">
-            <div className="isolate aspect-video w-96 rounded-xl bg-white/20 shadow-lg ring-1 ring-black/5">
-                    button
+            <div className="isolate aspect-video w-96 h-fit rounded-xl bg-white/20 shadow-lg ring-1 ring-black/5 px-5 py-10">
+                <div className="text-[#ff7a29] font-bold text-4xl font-mono ml-25">Signup</div>
+                <Inputbox Changehandler={(e)=>{console.log(e.target.value)}} type="text" placeholder="username" Title="Username"/>
+                <Inputbox Changehandler={(e)=>{console.log(e.target.value)}} type="text" placeholder="password" Title="Password"/>
+                <Inputbox Changehandler={(e)=>{console.log(e.target.value)}} type="text" placeholder="Email" Title="Email"/>
+                <Button children="Signup" clickhandler={()=>alert("hi")}/>
+                {/* SHould have a footer  */}
+                <div>
+
+                </div>
             </div>
         </div>
     )
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -4,15 +4,14 @@ import { ReactNode } from "react";
 
 interface ButtonProps {
   children: ReactNode;
-  className?: string;
-  appName: string;
+  clickhandler:()=>void
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+export const Button = ({ children,clickhandler}: ButtonProps) => {
   return (
     <button
-      className="ml-3 px-6 py-2 border-2 border-[#ff7a27] text-[#ff7a27] rounded-full font-semibold hover:bg-[#ff7a27] hover:text-white transition cursor-pointer w-50"
-      onClick={() => alert(`Hello from your ${appName} app!`)}
+      className="px-5 py-2 border-2 border-[#ff7a27] text-[#ff7a27] rounded-full font-semibold hover:bg-[#ff7a27] hover:text-white transition cursor-pointer w-full mt-7"
+      onClick={clickhandler}
     
     >
       {children}
