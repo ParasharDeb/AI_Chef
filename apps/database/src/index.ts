65ffdaead8648923387e6becf7822452a788ab8e
ParasharDeb <parashardeb11@gmail.com>
2026-05-03 11:02:47 +0530

    added mongoose database

---
+++ apps/database/src/index.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+dotenv.config()
+const schema=mongoose.Schema;
+const ObjectId=schema.ObjectId;
+export async function connectDB() {
+  if (mongoose.connection.readyState >= 1) return;
+
+  const uri = process.env.DATABASE_URL ;
+  console.log(uri)
+  if(!uri){
+    console.log("db not found")
+    return
+  }
+  await mongoose.connect(uri)
+  console.log("Mongo connected");
+}
+const admin=new schema({
+    name:{type:String,required:true},
+    password:{type:String,required:true}
+})
+const dish=new schema({
+    name:{type:String,required:true},
+    description:{type:String},
+    price:{type:Number,required:true},
+    image:{type:Number,required:true}
+})
+export const adminModel=mongoose.model('admin',admin);
+export const dishModel=mongoose.model('dish',dish)
